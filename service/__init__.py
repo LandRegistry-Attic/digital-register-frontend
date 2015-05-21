19226f3f5df0a8a82469f044d7a0b48a8153f902
lgonczar <gonczar@gmail.com>
2015-05-21 14:09:38 +0000

    Store application fault details in log file

--- service/__init__.py
+++ service/__init__.py
@@ -7,8 +7,9 @@ from flask_login import LoginManager
 from config import CONFIG_DICT
 from service import logging_config
 
-# This causes the traceback to be written to stderr in case of faults
-faulthandler.enable()
+# This causes the traceback to be written to the fault log file in case of serious faults
+fault_log_file = open(CONFIG_DICT['FAULT_LOG_FILE_PATH'], 'a')
+faulthandler.enable(file=fault_log_file)
 
 app = Flask(__name__)
 app.config.update(CONFIG_DICT)
