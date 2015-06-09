d8e8ff02af2d56d176b3b7d39e2351457266343e
Camille Baldock <camille@camillebaldock.co.uk>
2015-06-09 09:53:17 +0100

    Separate landregistry-specific SASS in separate folder

--- service/static/pipeline.py
+++ service/static/pipeline.py
@@ -6,12 +6,13 @@ import sass as libsass
 __dot = path.dirname(path.realpath(__file__))
 __toolkit_dir = path.join(__dot, 'govuk_frontend_toolkit/stylesheets/')
 __elements_dir = path.join(__dot, 'stylesheets/elements/')
+__landregistry_dir = path.join(__dot, 'stylesheets/landregistry/')
 
 def __compile_sass(_in, out, **kw):
     out.write(
         libsass.compile(
             string=_in.read(),
-            include_paths=[__toolkit_dir, __elements_dir]
+            include_paths=[__toolkit_dir, __elements_dir, __landregistry_dir]
         )
     )
 
