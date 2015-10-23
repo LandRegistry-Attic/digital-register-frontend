4bab9b2b6722f815f314bbe8a9823407e85a0040
Richard Prosser <richard.prosser@digital.landregistry.gov.uk>
2015-10-23 14:54:12 +0000

    Initial commit, following deletion (rename) of feature/feature/us122-pay-for-purchases

--- config.py
+++ config.py
@@ -33,9 +33,52 @@ CONFIG_DICT = {
     'TITLE_REGISTER_SUMMARY_PRICE': title_register_summary_price,
 }  # type: Dict[str, Union[bool, str, timedelta]]
 
-settings = os.environ.get('SETTINGS')
+# <worldpay> (From https://gh-svn-d01.diti.lr.net/svn/eservices/branches/release1415/ECBX_PortServicesBackEnd).
+WP_TEST_MODE_ON = 100
+WP_TEST_MODE_OFF = 0
 
-if settings == 'dev':
+WORLDPAY_DICT = {
+    'BYPASS_WORLDPAY': 'N',
+    'PURCHASE_TIME_EXPIRY_LIMIT': '5',
+    'WORLDPAY_REDIRECT_URL': 'https://secure.worldpay.com/wcc/dispatcher',
+    'WP_AUTH_CURR': 'GBP',
+    'WP_CALLBACK_PW': 'unknown',
+    'KEYSTORE_FILE_PATH': '/web/sycell/config/lronline.pfx',
+    'KEYSTORE_SECRET': 'trust',
+    'WP_INST_ID': os.getenv('WP_INST_ID', ''),
+    'WP_AUTH_MODE': 'E',
+    'WP_ACCOUNT_ID': os.getenv('WP_ACCOUNT_ID', ''),
+    'WP_TEST_MODE': WP_TEST_MODE_ON,
+    'WP_DEFAULT_COUNTRY': 'GB',
+    'WP_HOSTNAME_FOR_POST_AUTH': 'select.worldpay.com',
+    'WP_PORT_FOR_POST_AUTH': '443',
+    'WP_URLEND_FOR_POST_AUTH': '/wcc/authorise',
+    'WP_PASSWORD_FOR_POST_AUTH': os.getenv('WP_PASSWORD_FOR_POST_AUTH',''),
+    'WP_OP_FOR_POST_AUTH': 'postAuth-full',
+    'WP_TESTMODE_FOR_POST_AUTH': WP_TEST_MODE_ON,
+    'WP_INST_ID_FOR_POST_AUTH': os.getenv('WP_INST_ID_FOR_POST_AUTH', ''),
+    'WORLDPAY_START_ADDRESS': '195.35.90.0',
+    'WORLDPAY_FINISH_ADDRESS': '195.35.91.255',
+    'WP_AUTHORISATION_CALLBACK_URL': 'testpayment.landregisteronline.gov.uk/lro/servlet/AuthorisationCallbackServlet',
+    'WP_CALLBACK_SERVER_URL': 'http://localhost:10038',
+    'RT_CONTEXT ': ' "https://localhost:10035"',
+    'AVS_CHECK_ON': 'N',
+    'AVS_FAILED_MATCH': '4',
+    'BACKDOOR': 'false',
+    'CONFIRM_PAYMENT_URL': '/wps/myportal/QDMPS-ProtectedServlets/ConfirmPayment?',
+    'ACTION_URL1': '/download.do',
+    'ACTION_URL2': '/QuickEnquiryInit.do',
+    'ACTION_URL3': ''
+}
+#</worldpay>
+
+# N.B: we do not want 'live' by default - it should be set explicitly in the environment, as required.
+settings = os.getenv('SETTINGS', 'DEV').lower()
+
+if settings == 'live':
+    WORLDPAY_DICT['WP_TEST_MODE'] = WP_TEST_MODE_OFF
+    CONFIG_DICT['DEBUG'] = False
+elif settings == 'dev':
     CONFIG_DICT['DEBUG'] = True
 elif settings == 'test':
     # We do NOT set TESTING to True here as it turns off authentication, and we
