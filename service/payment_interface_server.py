4eae72c7f51c81c99e10ad7a36d8445cf71258f9
jonkennedy77 <jon.kennedy@digital.landregistry.gov.uk>
2015-11-05 11:04:49 +0000

    Added error handling. UNTESTED!

--- service/payment_interface_server.py
+++ service/payment_interface_server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, abort
 from config import WORLDPAY_DICT, ROOT_DIR
 
 tmpl_dir = os.path.join(ROOT_DIR, os.path.normpath('service/templates'))
@@ -14,13 +14,21 @@ def worldpay():
     Populate dict with user variables passed in, then re-post to Worldpay.
     """
 
-    # N.B.: "request.form" is a 'multidict', so need to flatten it first; assume single value per key.
-    WORLDPAY_DICT.update(request.form.to_dict())
 
-    # Convert to lower case, for use by template.
-    _worldpay_dict = dict((k.lower(), v) for k,v in WORLDPAY_DICT.items())
+    try:
+        # N.B.: "request.form" is a 'multidict', so need to flatten it first; assume single value per key.
+        WORLDPAY_DICT.update(request.form.to_dict())
 
-    return render_template('hiddenWP.html', worldpay_params=_worldpay_dict)
+        # Convert to lower case, for use by template.
+        _worldpay_dict = dict((k.lower(), v) for k,v in WORLDPAY_DICT.items())
+
+        return render_template('hiddenWP.html', worldpay_params=_worldpay_dict)
+
+    # Any generic errors should be handled elsewhere ...
+    except Exception as e:
+        # [Should have a log call here].
+        abort(500)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
