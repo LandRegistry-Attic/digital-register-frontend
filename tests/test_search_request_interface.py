cff64b21b8b2a03ddec25141667ce5a8f540ea4a
RichardProsser <richard.prosser@digital.landregistry.gov.uk>
2015-11-16 15:57:54 +0000

    Cosmetic changes.

--- tests/test_search_request_interface.py
+++ tests/test_search_request_interface.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 from unittest import mock
 from tests.fake_response import FakeResponse
-from service import property_search_interface
+from service import search_request_interface
 
 
 class TestPropertySearchInterface:
@@ -15,12 +15,12 @@ class TestPropertySearchInterface:
     timestamp = str(datetime.datetime.now())       # This has the same format as the DB2 version.
     b_timestamp = bytes(timestamp, 'utf-8')        # 'bytes' required for Mock.
 
-    @mock.patch.object(property_search_interface.requests, 'post', return_value=FakeResponse(b_timestamp, 201))
+    @mock.patch.object(search_request_interface.requests, 'post', return_value=FakeResponse(b_timestamp, 201))
     def test_property_search_interface_returns_datetime_string_from_api_when_response_is_201(self, mock_post):
-        result = property_search_interface.insert('title123', '1.20', '00 Test Road, Test Town, Plymouth, PL1 1AB')
+        result = search_request_interface.insert('title123', '1.20', '00 Test Road, Test Town, Plymouth, PL1 1AB')
         assert result == self.timestamp
 
-    @mock.patch.object(property_search_interface.requests, 'post', return_value=FakeResponse(None, 200))
+    @mock.patch.object(search_request_interface.requests, 'post', return_value=FakeResponse(None, 200))
     def test_property_search_interface_returns_none_from_api_when_response_is_200(self, mock_post):
-        result = property_search_interface.update("MGIRDLER", self.timestamp, "2123652039 ")
+        result = search_request_interface.update("MGIRDLER", self.timestamp, "2123652039 ")
         assert result is None
