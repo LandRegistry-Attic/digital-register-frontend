fdf6bccfee8b089cd79e73d8e9adc54e3e83e70b
RichardProsser <richard.prosser@digital.landregistry.gov.uk>
2015-11-18 14:49:47 +0000

    Valid 'mocking'.

--- tests/test_confirm_selection.py
+++ tests/test_confirm_selection.py
@@ -1,13 +1,13 @@
 import os
-import mock
+import pytest  # type: ignore
+import json
+from unittest import mock
 from tests.fake_response import FakeResponse
-from service.server import confirm_selection, api_client
+from service.server import confirm_selection, api_client, app, _get_register_title
 from tests.test_search_request_interface import b_timestamp
 from service import search_request_interface
 from config import ROOT_DIR
 
-
-fake_title_path = os.path.join(ROOT_DIR, os.path.normpath('tests/data/fake_title.json'))
 
 search_results_html = \
 """
@@ -31,6 +31,10 @@ search_results_html = \
 </body>
 </html>
 """
+# Get a fake title, for use by mocked api_client.get_title().
+fake_title_path = os.path.join(ROOT_DIR, os.path.normpath('tests/data/fake_title.json'))
+fake_title = open(fake_title_path).read()                       # (May raise an error; handled by pytest).
+fake_response = FakeResponse(str.encode(fake_title), 200)       # N.B. Response.content is 'bytes'.
 
 class TestConfirmSelection:
     """
@@ -40,20 +44,17 @@ class TestConfirmSelection:
         * api_client.get_title (used by _get_register_title)
         * search_request_interface
     """
+    def setup_class(cls):
+        cls.app = app.test_client()
 
-    # Get a fake title, for use by mocked api_client.get_title().
-    # N.B.: fake_title is a class-level variable, as 'pytest' discourages the use of __init__()!
-    fake_title = open(fake_title_path).readlines()      # (May raise an error; handled by pytest).
-
-    # TODO: invoke 'confirm_selection()'
 
     # Use tests/data/fake_title.json as a dummy title.
-    @mock.patch.object(api_client, 'get_title', return_value=fake_title)
+    @mock.patch('service.server.api_client.get_title', return_value=fake_response.json())
     @mock.patch.object(search_request_interface.requests, 'post', return_value=FakeResponse(b_timestamp, 201))
-    def test_confirm_selection_renders_valid_form(self, mock_title, mock_post):
-        result = confirm_selection("DN1000", "LU1 1DZ")
+    def test_confirm_selection_renders_valid_form_when_response_is_200(self, mock_get, mock_post):
+        response = self.app.get('/confirm-selection/DN1000/LU1%201DZ')
+        assert response.status_code == 200
 
 
 if __name__ == '__main__':
-    result = TestConfirmSelection()
-    import pdb; pdb.set_trace()
+    pytest.main()
