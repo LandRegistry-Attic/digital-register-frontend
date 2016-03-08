02ff987f6570b3ff2cb1d76d0d36d60f2beeec36
Pheonnexx <pheonn@hotmail.co.uk>
2016-03-08 10:35:04 +0000

    refactor - remove unnecessary sign in form

--- service/forms.py
+++ service/forms.py
@@ -3,15 +3,6 @@ from wtforms.fields import StringField, PasswordField  # type: ignore
 from wtforms.validators import Required, Length        # type: ignore
 
 
-class SigninForm(Form):  # type: ignore
-    username = StringField('username', [Required(message='Username is required'),
-                                        Length(min=4, max=70, message='Username is incorrect')])
-    password = PasswordField('password', [Required(message='Password is required')])
-
-    def __init__(self, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
-
-
 class TitleSearchForm(Form):  # type: ignore
     search_term = StringField('search_term',
                               [Required(message='Search term is required'),
