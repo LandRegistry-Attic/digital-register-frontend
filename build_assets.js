3392b88468b35957a2b1954ccd54628fd38451b7
Andy Mantell <andymantell@hotmail.com>
2016-03-08 15:15:47 +0000

    Initial commit of build script to import assets from the land-registry-elements pattern library

---
+++ build_assets.js
@@ -0,0 +1,19 @@
+var copy = require('ncp').ncp;
+
+require('land-registry-elements').then(function(assetPath) {
+
+  var destPath = 'service/static/assets';
+
+  copy(assetPath, destPath, function(err, files) {
+    if(err) {
+      reject(err);
+      return;
+    }
+
+    console.log('Assets copied to', destPath);
+  });
+})
+.catch(function(e) {
+  console.error(e);
+});
+
