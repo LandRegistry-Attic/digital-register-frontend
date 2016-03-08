038b0a45cc38b10bfcdbba2a77b489e2ac92b415
Andy Mantell <andymantell@hotmail.com>
2016-03-08 15:21:05 +0000

    Removing old assets

--- build_assets.js
+++ build_assets.js
@@ -2,7 +2,7 @@ var copy = require('ncp').ncp;
 
 require('land-registry-elements').then(function(assetPath) {
 
-  var destPath = 'service/static/assets';
+  var destPath = 'service/static/land-registry-elements';
 
   copy(assetPath, destPath, function(err, files) {
     if(err) {
