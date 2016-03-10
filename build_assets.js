d0ee2c066af1355dd23de880ae36b59eb30678ec
Andy Mantell <andymantell@hotmail.com>
2016-03-10 09:19:03 +0000

    Specify the correct components to build for the digital-register-frontend rather than requesting everything

--- build_assets.js
+++ build_assets.js
@@ -3,7 +3,15 @@ var buildAssets = require('/vagrant/apps/land-registry-elements');
 
 buildAssets({
   'destination': path.resolve(__dirname, 'service/static/land-registry-elements'),
-  'mode': 'production'
+  'mode': 'production',
+  'components': [
+    'pages/drv/landing-form',
+    'pages/drv/search-form',
+    'pages/drv/search-results',
+    'pages/drv/order-confirmation',
+    'pages/drv/summary',
+    'pages/land-registry/error-page'
+  ]
 })
   .then(function(dest) {
     console.log('Done');
