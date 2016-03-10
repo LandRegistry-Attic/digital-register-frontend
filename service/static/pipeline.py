5c341c1ca03546a3ed9e4d00a2582947e0b80b0e
Andy Mantell <andymantell@hotmail.com>
2016-03-10 09:09:57 +0000

    Fix incorrect paths in flask pipeline

--- service/static/pipeline.py
+++ service/static/pipeline.py
@@ -2,33 +2,33 @@ from flask.ext.assets import Bundle  # type: ignore
 from os import path                  # type: ignore
 
 
-css = Bundle('land-registry-elements/stylesheets/govuk-template.css',
-             'land-registry-elements/stylesheets/fonts.css',
-             'land-registry-elements/stylesheets/elements.css',
+css = Bundle('land-registry-elements/assets/stylesheets/govuk-template.css',
+             'land-registry-elements/assets/stylesheets/fonts.css',
+             'land-registry-elements/assets/stylesheets/elements.css',
               output='dist/css/main.css')
 
-css_print = Bundle('land-registry-elements/stylesheets/govuk-template-print.css',
+css_print = Bundle('land-registry-elements/assets/stylesheets/govuk-template-print.css',
                     output='dist/css/print.css')
 
-css_ie8 = Bundle('land-registry-elements/stylesheets/govuk-template-ie8.css',
-                 'land-registry-elements/stylesheets/fonts-ie8.css',
-                 'land-registry-elements/stylesheets/elements-ie8.css',
+css_ie8 = Bundle('land-registry-elements/assets/stylesheets/govuk-template-ie8.css',
+                 'land-registry-elements/assets/stylesheets/fonts-ie8.css',
+                 'land-registry-elements/assets/stylesheets/elements-ie8.css',
                   output='dist/css/main-ie8.css')
 
-css_ie7 = Bundle('land-registry-elements/stylesheets/govuk-template-ie7.css',
-                 'land-registry-elements/stylesheets/elements-ie7.css',
+css_ie7 = Bundle('land-registry-elements/assets/stylesheets/govuk-template-ie7.css',
+                 'land-registry-elements/assets/stylesheets/elements-ie7.css',
                   output='dist/css/main-ie7.css')
 
-css_ie6 = Bundle('land-registry-elements/stylesheets/govuk-template-ie6.css',
-                 'land-registry-elements/stylesheets/elements-ie6.css',
+css_ie6 = Bundle('land-registry-elements/assets/stylesheets/govuk-template-ie6.css',
+                 'land-registry-elements/assets/stylesheets/elements-ie6.css',
                   output='dist/css/main-ie6.css')
 
-js_ie = Bundle('land-registry-elements/javascripts/ie.js',
-                      'land-registry-elements/javascripts/polyfills.js',
-                      filters='rjsmin', output='dist/javascript/ie.js')
+js_ie = Bundle('land-registry-elements/assets/javascripts/ie.js',
+               'land-registry-elements/assets/javascripts/polyfills.js',
+               filters='rjsmin', output='dist/javascript/ie.js')
 
 
-js = Bundle('land-registry-elements/javascripts/govuk-template.js',
-            'land-registry-elements/javascripts/leaflet.js',
-            'land-registry-elements/javascripts/landregistry.js',
+js = Bundle('land-registry-elements/assets/javascripts/govuk-template.js',
+            'land-registry-elements/assets/javascripts/leaflet.js',
+            'land-registry-elements/assets/javascripts/landregistry.js',
             filters='rjsmin', output='dist/javascript/main.js')
