90989b31cf606be8c796b02184a5cea9c9a45e28
Pheonnexx <pheonn@hotmail.co.uk>
2016-05-23 11:53:42 +0100

    Add confirm selection form

--- service/forms.py
+++ service/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import Form                                          # type: ignore
-from wtforms.fields import StringField, PasswordField, RadioField, SubmitField   # type: ignore
-from wtforms.validators import InputRequired, Length                     # type: ignore
+from flask_wtf import Form                                                      # type: ignore
+from wtforms.fields import StringField, RadioField, SubmitField, BooleanField   # type: ignore
+from wtforms.validators import InputRequired, Required                          # type: ignore
 
 
 class TitleSearchForm(Form):  # type: ignore
@@ -16,6 +16,13 @@ class LandingPageForm(Form):  # type: ignore
                              choices=[('title_summary', 'Title summary'), ('full_title_documents', 'Full title documents'), ('official_copy', 'Official copy')])
 
     submit = SubmitField("Continue")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+
+class ConfirmTermsConditionsForm(Form):  # type: ignore
+    right_to_cancel = BooleanField('right_to_cancel', validators=[Required(message='Please choose an option.')])
 
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
